d5793fa84ba89b15ce6921b676da07b1b43011c9
eldad-assaf <eldadux@gmail.com>
2023-12-31 12:13:01 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/open_mail_and_phone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.open_mail_and_phone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+                onPressed: () async {
+                  final url = Uri.parse(
+                      'mailto:example@developerxon.com?subject=Test&body=Test email');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: Icon(Icons.mail_lock)),
+            IconButton(
+                onPressed: () async {
+                  final url = Uri.parse('tel:+123 456 789');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: Icon(Icons.phone))
+          ],
+        ),
+      ),
+    );
+  }
+}
